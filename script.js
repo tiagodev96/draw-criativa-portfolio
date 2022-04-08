5871ded38bda02459b3c8067b2057f6d6355a5bd
Tiago Batista <99134069+tiagodev96@users.noreply.github.com>
2022-04-08 10:32:22 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const rootElement = document.getElementById("menu-wrapper");
+const menu = MenuHamburger.initialize({
+  rootElement,
+  size: 30,
+  backgroundColor: "#f8f9fa",
+});
+const dropmenu = document.querySelector("#dropdownmenu");
+
+menu.on("toggle", () => {
+  dropmenu.classList.toggle("active");
+});
+
+/* GRID PORTFOLIO */
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+  },
+});
