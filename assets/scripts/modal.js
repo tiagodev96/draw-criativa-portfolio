9d654509abb8f53a70f968a438ba2cbdbe77dff0
Tiago Batista <99134069+tiagodev96@users.noreply.github.com>
2022-04-11 23:47:28 -0300

    All modals increased

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,14 +1,4 @@
-const modalProjetoDeMaquinas = document.querySelector(
-  "#modal-projeto-de-maquinas"
-);
-
 const modals = document.querySelectorAll(".modal");
-
-function openProjetoDeMaquinasModal() {
-  modalProjetoDeMaquinas.classList.remove("hide");
-  window.scrollTo(0, 0);
-  window.addEventListener("scroll", noScroll);
-}
 
 function noScroll() {
   window.scrollTo(0, 0);
@@ -21,15 +11,136 @@ function closeModal() {
   window.removeEventListener("scroll", noScroll);
 }
 
-var swiper = new Swiper(".modal-swipper", {
-  loop: true,
+//* modal projetos de máquinas
+const modalProjetoDeMaquinas = document.querySelector(
+  "#modal-projeto-de-maquinas"
+);
+
+function openProjetoDeMaquinasModal() {
+  modalProjetoDeMaquinas.classList.remove("hide");
+  window.scrollTo(0, 0);
+  window.addEventListener("scroll", noScroll);
+}
+
+var swiper = new Swiper(".modal-projetos-maquinas", {
   spaceBetween: 10,
   slidesPerView: 4,
   freeMode: true,
   watchSlidesProgress: true,
 });
-var swiper2 = new Swiper(".modal-swipper2", {
-  loop: true,
+var swiper2 = new Swiper(".modal-projetos-maquinas2", {
+  lazy: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//* modal dispositivos
+const modalDispositivos = document.querySelector("#modal-dispositivos");
+
+function openDispositivosModal() {
+  modalDispositivos.classList.remove("hide");
+  window.scrollTo(0, 0);
+  window.addEventListener("scroll", noScroll);
+}
+
+var swiper = new Swiper(".modal-dispositivos", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".modal-dispositivos2", {
+  lazy: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//* modal adaptação
+const modalAdaptacao = document.querySelector("#modal-adaptacao");
+
+function openAdaptacaoModal() {
+  modalAdaptacao.classList.remove("hide");
+  window.scrollTo(0, 0);
+  window.addEventListener("scroll", noScroll);
+}
+
+var swiper = new Swiper(".modal-adaptacao", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".modal-adaptacao2", {
+  lazy: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//* modal layout 3D
+const modalLayout3D = document.querySelector("#modal-layout3d");
+
+function openLayout3DModal() {
+  modalLayout3D.classList.remove("hide");
+  window.scrollTo(0, 0);
+  window.addEventListener("scroll", noScroll);
+}
+
+var swiper = new Swiper(".modal-layout3d", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".modal-layout3d2", {
+  lazy: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+//* modal NR12
+const modalNR12 = document.querySelector("#modal-NR12");
+
+function openNR12Modal() {
+  modalNR12.classList.remove("hide");
+  window.scrollTo(0, 0);
+  window.addEventListener("scroll", noScroll);
+}
+
+var swiper = new Swiper(".modal-NR12", {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper2 = new Swiper(".modal-NR122", {
   lazy: true,
   spaceBetween: 10,
   navigation: {
